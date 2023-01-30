a4f0c9f95262ee3dc540f09ad39012c8329b54b1
ChloeM <meinshac@gmail.com>
2023-01-30 03:03:51 -0500

    add input variable and couple other comments

--- openlibrary-data-chunk-process.py
+++ openlibrary-data-chunk-process.py
@@ -1,9 +1,9 @@
 # With this file you can convert your txt files into smaller files which are easier to load into the db
-#First, decide how you want to identify the files. I used author/editions/works
+#First, decide how you want to identify the files. I used author/editions/works so files end up looking like author_3000000, editions_3000000
 #Second, decide how large you would like to make each chunk.  - in the edition table I made them 3 million lines which was about 3.24 gigs and ended up taking about an hour to load.
-
+input = 'C:\openlibrary-search\data\ol_dump_authors_2016-07-31_processed.csv'
 file_identifer = 'author'
-lines_per_file = 8
+lines_per_file = 3000000
 filenames = []
 csvoutputfile = None
 with open(input) as cvsinputfile:
